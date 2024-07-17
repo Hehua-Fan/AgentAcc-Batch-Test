06a0cf30a50ad90aab41873f2039fa65255e60a4
Hehua Fan <forhheart5532@gmail.com>
2024-07-17 14:57:07 +0800

    playground

--- playground/test_qa_pair_generator.py
+++ playground/test_qa_pair_generator.py
@@ -1,21 +1,20 @@
 import pandas as pd
 from zhipuai import ZhipuAI
 import json
-from config import ZHIPU_AI_API_KEY
 
-def qa_pair_generator(ZHIPU_AI_API_KEY, question, answer, num_group=5, context=""):
+def qa_pair_generator(ZHIPU_AI_API_KEY, question, answer, num_group, context):
     system_prompt = """
     # Role：请根据我的主题和问题，帮我生成我想要的问答对
     # Format
-    请以json格式输出，以Question, Answer作为Key，直接书写问题和回答，不需要冒号，例如：
+    请以json格式输出，以Question, Answer作为Key，以下是示例，只是在向你展示格式：
     {
         "Question": [
-            "问题1",
-            "问题2"
+            "为什么我的服务这么慢？",
+            "我什么时候能收到我订购的商品？"
         ],
         "Answer": [
-            "回答1",
-            "回答2"
+            "很抱歉给您带来了不便，我们会立即为您查看原因并尽快解决，请您提供一下相关信息以便我们更好地帮助您。",
+            "感谢您的耐心等待，根据我们的系统显示，您的商品将在接下来的3-5天内送达。如果有任何问题，我们会及时与您联系。"
         ]
     }   
     """
@@ -37,17 +36,19 @@ def qa_pair_generator(ZHIPU_AI_API_KEY, question, answer, num_group=5, context="
     )
     
     response_str = response.choices[0].message.content
-    response_json = json.loads(response_str)
-    
-    df = pd.DataFrame(response_json)
+    response_str_extracted = response_str.split('`json')[1].split('`')[0].strip()
+    response_json = json.loads(response_str_extracted)
+    df = pd.DataFrame.from_dict(response_json)
     
     return df
 
+
 if __name__ == '__main__':
+    ZHIPU_AI_API_KEY = ""
     num_group = 5
     context = "这个电话客服是基于线下实体店的，是线下类似于剧本杀，棋牌游戏的服务行业"
     question = "客户的电话投诉"
     answer = "标准而礼貌的客服回复"
-
-    df = qa_pair_generator(ZHIPU_AI_API_KEY, question, answer, num_group=5, context="")
+    df = qa_pair_generator(ZHIPU_AI_API_KEY, question, answer, num_group, context)
+    df.to_csv('qa_pairs.csv', index=False, encoding='utf-8-sig')
     print(df)
