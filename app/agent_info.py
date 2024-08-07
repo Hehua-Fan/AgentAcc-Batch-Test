8c0671a246e85d1c974136ecbd35af2856923f71
Hehua Fan <forhheart5532@gmail.com>
2024-08-07 07:39:00 -0400

    update layout

--- app/agent_info.py
+++ app/agent_info.py
@@ -4,7 +4,7 @@ from utils import parse_info
 def agent_info():
     with st.expander("🤖 Agent信息填写"):
         st.write("**Agent信息查询**")
-        st.write("(我的Agent - 发布 - API服务)")
+        st.write("**我的Agent - 发布 - API服务**")
 
         options = ["uat", "test", "lingda"]
         platform = st.radio(
